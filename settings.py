c27a4ea337209a7f6201b65199267aec4745bf64
HanBeom98 <Carry9691@gmail.com>
2025-03-27 13:38:31 +0900

    refactor: 코인당 최대 투자 금액 수정

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ TRADE_TICKERS = ['AVAX', 'PENDLE', 'SUI', 'XRP', 'SOL', 'ATOM']
 
 # 매매 조건 설정
 MAX_TOTAL_INVEST = 1500000     # 전체 투자금 한도
-MAX_INVEST_AMOUNT = 400000      # 코인당 최대 투자 금액
+MAX_INVEST_AMOUNT = 300000      # 코인당 최대 투자 금액
 MIN_ORDER_AMOUNT = 5000         # 최소 주문 금액
 MAX_INVEST_PER_TICKER_RATIO = 0.3
 #COOLDOWN_TIME = 300             # 쿨다운 시간 (초)
